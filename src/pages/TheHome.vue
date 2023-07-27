d639247bb8dd16812c10ed746055d58e18a95456
Karlo Dela Rosa <jrosa@salespond.com>
2023-07-27 23:32:49 +0800

    Added new design for mobile

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,9 +1,80 @@
 <script setup lang="ts">
+import ArrowDown from '../components/svg/ArrowDown.vue'
+import BigArrowDown from '../components/svg/BigArrowDown.vue'
+import MenuIcon from '../components/svg/MenuIcon.vue'
+import Amplifier from '../components/svg/Amplifier.vue'
+import GameController from '../components/svg/GameController.vue'
+import Television from '../components/svg/Television.vue'
 
+const logo = '/logo.png'
+const about = '/about.png'
 </script>
 
 <template>
-  <div>
-    hello there
-  </div>
+  <main class="w-full ">
+    <div class="min-h-screen">
+      <div class="bg-black h-screen py-[10px] px-[20px] relative">
+
+        <!-- Nav -->
+        <div class="flex flex-row justify-between items-center">
+          <div class="flex flex-row items-center gap-2">
+            <img :src="logo" class="w-[50px]">
+            <h1 class="text-white text-lg">karlodr.dev</h1>
+          </div>
+          <MenuIcon/>
+        </div>
+        <!-- End of Nav -->
+
+        <h1 class="hero-text">JKDR</h1>
+
+        <ArrowDown class="absolute bottom-10 right-10 animate-bounce"/>
+      </div>
+    </div>
+
+    <div class="min-h-screen bg-black py-[50px]">
+      <img :src="about" alt="">
+
+      <div class="pt-[50px] px-[20px] mb-[80px]">
+        <h2 class="section-title text-white font-extra-bold text-center mb-[40px]">ABOUT</h2>
+        <p class="text-white mb-[30px] text-[18px] font-light">Hi! I am Karlo, problem solver, explorer, and passionate software developer, and also a Frontend developer who loves to create design that will users enjoy.</p>
+        <p class="text-white mb-[50px] text-[18px] font-light">I love games, coffee, anime, and dogs!</p>
+
+        <div class="flex flex-row justify-between px-[15px]">
+          <GameController/>
+          <Amplifier/>
+          <Television/>
+        </div>
+      </div>
+
+      <BigArrowDown class="ml-auto"/>
+    </div>
+
+    <div class="min-h-screen bg-black py-[50px] px-[20px]">
+      <h2 class="section-title text-white font-extra-bold text-left">EXPERTISE</h2>
+
+      <div>
+        
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.hero-text {
+  text-orientation: upright;
+  writing-mode: vertical-rl;
+  color: white;
+  font-size: 170px;
+  font-weight: 900;
+  letter-spacing: -60px;
+  line-height: 1;
+  text-shadow: rgba(255,255,255,0.4) 0px 0px 72px;
+}
+
+.section-title {
+  font-size: 70px;
+  letter-spacing: 13px;
+  word-break: break-all;
+  line-height: 1;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,39 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontWeight: {
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      'extra-bold': '800',
+      black: '900',
+    },
+    extend: {
+      colors: {
+        'primary-1': '#008AFE',
+        'primary-2': '#5CDDD4',
+        'primary-3': '#9EEDE3',
+        'primary-4': '#000000',
+        'secondary': '#346DD8',
+        'neutral-1': '#3D3D3D',
+        'neutral-2': '#848B90',
+        'neutral-3': '#BDC5CD',
+        'neutral-4': '#EBF0F5',
+        'neutral-5': '#F6F9FC',
+        'neutral-6': '#6D6E70',
+        'disabled-text-1': '#939597',
+        'disabled-1': '#DBDCDE',
+
+        'error-1': '#EB5757',
+
+        // Dark Mode
+        'custom-dark': '#282828',
+      },
+    },
   },
   plugins: [],
 }
