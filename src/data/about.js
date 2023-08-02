9a52c9822da4d20ef778294b66ef2f259fe39f2c
Karlo Dela Rosa <jrosa@salespond.com>
2023-08-02 12:51:16 +0800

    Edit sentence

--- src/data/about.js
+++ src/data/about.js
@@ -1,4 +1,4 @@
 export const ABOUT_DATA = {
-  main_description: 'Hi! I am Karlo, problem solver, explorer, passionate software developer, and also a Frontend developer who loves to create design that will users enjoy.',
+  main_description: 'Hi! I am Karlo, a problem solver, explorer, and passionate software developer. I am also a frontend developer who loves creating designs that users will enjoy.',
   sub_description: 'I love games, coffee, anime, and dogs!'
 }
