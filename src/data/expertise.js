fed2a7ae82275b0406dfe6a86b6de3894847d768
Karlo Dela Rosa <karlodelarosa@Karlos-MacBook-Air.local>
2023-08-10 08:58:24 +0800

    Added expertise

--- src/data/expertise.js
+++ src/data/expertise.js
@@ -8,6 +8,10 @@ export const EXPERTISE_DATA = [
     description: 'Plan and design the application infrastructure, develop features, conduct code reviews, and handle deployments.'
   },
   {
+    title: 'Software Architect',
+    description: 'Develops project-specific technical requirements and establishes technical standards for tools and platforms. Constructs automated processes, including deployment and development environments.'
+  },
+  {
     title: 'Software Solution',
     description: 'Provide software solutions by analyzing business requirements and creating technical specifications that meet the client\'s needs with an up-to-date approach.'
   },
